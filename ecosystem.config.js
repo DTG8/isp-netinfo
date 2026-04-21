210a788586711380e5b1b918bc17d1278d0d1728
Daniel Mobolade <114698715+DTG8@users.noreply.github.com>
2026-04-21 11:50:08 +0100

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: 'isp-netinfo',
+      script: 'server.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 6666
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 6666
+      }
+    }
+  ]
+};
