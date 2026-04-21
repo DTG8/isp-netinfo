e41e243fddfe010e683ccd1f4708fb0bc8df20fe
Daniel Mobolade <114698715+DTG8@users.noreply.github.com>
2026-04-21 11:58:18 +0100

    changed port number

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,11 +10,11 @@ module.exports = {
       max_memory_restart: '1G',
       env: {
         NODE_ENV: 'development',
-        PORT: 6666
+        PORT: 6060
       },
       env_production: {
         NODE_ENV: 'production',
-        PORT: 6666
+        PORT: 6060
       }
     }
   ]
